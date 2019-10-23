0c39d37fa38b90c726659d3f07bb342d330efa63
gkgaurav31 <gkgaurav31@gmail.com>
2019-10-23 19:34:21 +0530

    java directory listening

---
+++ src/main/java/com/test/MyServlet.java
@@ -0,0 +1,56 @@
+package com.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		 String dirName = "C:\\Users\\gauk\\Desktop\\Temp\\to_delete";
+	     
+		 PrintWriter out = response.getWriter();
+		 
+	        File fileName = new File(dirName);
+	        File[] fileList = fileName.listFiles();
+	        
+	        
+	        for (File file: fileList) {
+	            
+	            System.out.println(file);
+	            out.println(file);
+	            
+	        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
